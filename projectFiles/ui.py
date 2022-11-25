d76124029ab986e78ebd0598508021329021d509
JiyuHe <he_jiyu@163.com>
2022-11-26 02:06:45 +0800

    重新搭建UI框架，运行测试的功能

--- projectFiles/ui.py
+++ projectFiles/ui.py
@@ -4,17 +4,42 @@ import datetime
 from time import strftime
 
 from PyQt5.QtWidgets import QMessageBox, QMainWindow, QSpinBox, QApplication, QFileSystemModel, QFileDialog
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QThread, pyqtSignal
 from UI.mainWindow import Ui_MainWindow
+from workspace import Workspace
+from myModel import Model
 
+
+class EvaluateThread(QThread):
+    # sig = pyqtSignal(bool)
+
+    def __init__(self, parent=None):
+        super(EvaluateThread, self).__init__(parent)
+
+    def __del__(self):
+        self.wait()
+
+    def set_workspace(self, workspace:Workspace):
+        self.image_folder = workspace.get_image_folder()
+        self.result_folder = workspace.get_result_folder()
+        self.model_index = workspace.get_model_index()
+
+    def run(self):
+        model = Model()
+        if self.model_index == 1:
+            model.load_model("model/cnn_24.pt")
+        model.load_data(self.image_folder)
+        model.run_model(self.result_folder)
+        # self.sig.emit(True)
 
 class MainWindow(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.workdir = None
         self.setupUi(self)
-        self.treeView.hide()
-        self.widget.hide()
+        self.workspace = Workspace()
+        # self.workdir = None
+        # self.treeView.hide()
+        # self.widget.hide()
 
     def q_action(self, q):
         """
@@ -22,6 +47,7 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         @para:
         q：事件
         """
+        return
         if q == self.action_open_workdir or q == self.action_new_workdir:
             # 打开/新建工作区
             temp = QFileDialog.getExistingDirectory()
@@ -35,8 +61,8 @@ class MainWindow(Ui_MainWindow, QMainWindow):
                 # 初始化工作区
                 os.mkdir(self.workdir)
                 os.mkdir(self.workdir + "/data")
-                os.mkdir(self.workdir + "/data/train")
-                os.mkdir(self.workdir + "/data/test")
+                # os.mkdir(self.workdir + "/data/train")
+                # os.mkdir(self.workdir + "/data/test")
                 os.mkdir(self.workdir + "/model")
                 os.mkdir(self.workdir + "/result")
             # 更新工作区视图
@@ -57,10 +83,82 @@ class MainWindow(Ui_MainWindow, QMainWindow):
             self.treeView.hide()
             self.widget.hide()
 
+    def select_image_folder(self):
+        temp = QFileDialog.getExistingDirectory()
+        if temp == "":
+            return
+        else:
+            self.lineEdit_new_image_folder.setText(temp)
+            self.workspace.set_image_folder(temp)
 
+    def select_label_folder(self):
+        temp = QFileDialog.getExistingDirectory()
+        if temp == "":
+            return
+        else:
+            self.lineEdit_new_label_folder.setText(temp)
+            self.workspace.set_label_folder(temp)
+
+    def create_project(self):
+        """
+        创建新项目
+        """
+        temp = self.lineEdit_new_project_name.text()
+        if temp != "" and self.workspace.check():
+            self.workspace.set_project_name(temp)
+            self.lineEdit_project_name.setText(self.workspace.get_project_name())
+            self.lineEdit_image_folder.setText(self.workspace.get_image_folder())
+            self.lineEdit_label_folder.setText(self.workspace.get_label_folder())
+            self.stackedWidget.setCurrentIndex(1)
+            self.workspace.save_project()
+        else:
+            print("请填写完整信息")
+            # TODO 弹窗提醒
+
+    def modify_project_name(self):
+        temp = self.lineEdit_project_name.text()
+        if temp != "":
+            self.workspace.set_project_name(temp)
+
+    def modify_image_folder(self):
+        temp = QFileDialog.getExistingDirectory()
+        if temp == "":
+            return
+        else:
+            self.lineEdit_image_folder.setText(temp)
+            self.workspace.set_image_folder(temp)
+
+    def modify_label_folder(self):
+        temp = QFileDialog.getExistingDirectory()
+        if temp == "":
+            return
+        else:
+            self.lineEdit_label_folder.setText(temp)
+            self.workspace.set_label_folder(temp)
+
+    def model_chosen(self,index:int):
+        if index == 0:
+            self.textEdit_model_explaination.setText("请选择一个模型")
+        elif index == 1:
+            self.textEdit_model_explaination.setText("U-net的描述")
+        self.workspace.set_model_index(index)
+
+    def select_result_folder(self):
+        temp = QFileDialog.getExistingDirectory()
+        if temp == "":
+            return
+        else:
+            self.lineEdit_result_folder.setText(temp)
+            self.workspace.set_result_folder(temp)
+
+
+    def evaluate(self):
+        evaluate_thread.set_workspace(self.workspace)
+        evaluate_thread.start()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    evaluate_thread = EvaluateThread()
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
---
+++ projectFiles/workspace.py
@@ -0,0 +1,60 @@
+class Workspace:
+    """
+    工作区，用于存放和操作各种项目数据
+    """
+
+    def __init__(self):
+        self.project_name = None
+        self.image_folder = None
+        self.label_folder = None
+        self.model_index = 0
+
+    def set_project_name(self, project_name: str):
+        self.project_name = project_name
+        print(project_name)
+
+    def set_image_folder(self, image_folder: str):
+        self.image_folder = image_folder
+        print(self.image_folder)
+
+    def set_label_folder(self, label_folder: str):
+        self.label_folder = label_folder
+        print(self.label_folder)
+    
+    def set_result_folder(self, result_folder: str):
+        self.result_folder = result_folder
+        print(self.result_folder)
+
+    def set_model_index(self,model_index:int):
+            self.model_index = model_index
+
+    def get_project_name(self):
+        return self.project_name
+
+    def get_image_folder(self):
+        return self.image_folder
+
+    def get_label_folder(self):
+        return self.label_folder
+    
+    def get_result_folder(self):
+        return self.result_folder
+    
+    def get_model_index(self):
+        return self.model_index
+    
+    def save_project(self):
+        pass
+        # TODO
+    
+    def load_project(self):
+        pass
+        # TODO
+    
+    
+
+    def check(self):
+        if self.image_folder is not None and self.label_folder is not None:
+            return True
+        else:
+            return False
