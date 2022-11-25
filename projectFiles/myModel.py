bd55c9334278923c19cfc38162c0c4a427b0b54a
kaoxing <3032738029@qq.com>
2022-11-25 20:42:54 +0800

    训练部分代码

--- projectFiles/myModel.py
+++ projectFiles/myModel.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from PIL import Image
-
+import torch.nn as nn
 from myDataSetPre import MyDataSetPre
 from myDataSetTra import MyDataSetTra
 
@@ -13,11 +13,13 @@ class Model:
 
     def __init__(self):
         self.model = None
+        self.model_path = None
         self.predict_dataset = None
         self.train_dataset = None
 
     def load_model(self, load_path):
         """加载模型,参数（模型路径）"""
+        self.model_path = load_path
         self.model = torch.load(load_path, map_location='cuda' if torch.cuda.is_available() else 'cpu')
 
     def load_predict_data(self, data_path):
@@ -48,36 +50,41 @@ class Model:
             img = np.array(predict, dtype='uint8')
             cv2.imwrite("{0}/{1:0>3d}.png".format(predict_result_path, i), img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
-    def train(self, train_result_path, epoch, batch_size, shuffle=True, cover=False, model_name=None):
-        """训练模型,参数（训练结果,训练轮数, ）"""
+    def train(self, train_result_path, epoch, batch_size, learning_rate=0.00000001,
+              shuffle=True, optim="Adam", model_name=None):
+        """训练模型,参数（训练结果,训练轮数,训练批次大小,学习率,数据集是否打乱,model保存名），若新model名为空则将覆盖原model"""
         dataloader = DataLoader(
-            dataset=data_set,
+            dataset=self.predict_dataset,
             batch_size=batch_size,
-            shuffle=True,
+            shuffle=shuffle,
         )
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         loss_function = nn.BCELoss()
         loss_function = loss_function.to(device)
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.00000001)
-        count = 50
-
-        for cnt in range(count):
+        optimizer = None
+        if optim == "Adam":
+            optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
+        elif optim == "SGD":
+            optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
+        elif optim == "RMSProp":
+            optimizer = torch.optim.RMSprop(self.model.parameters(), lr=learning_rate)
+        for cnt in range(epoch):
             Loss = 0
-            print('第%d轮迭代' % cnt)
+            # print('第%d轮迭代' % cnt)
             for i, data in enumerate(dataloader):
                 input_data, labels = data
-                input_data = input_data.to(device)
-                labels = labels.to(device)
-                # cv2.imwrite("Test/input_data_%03d.png" % i,torch.reshape(input_data, (512, 512)).cpu().detach().numpy() * 255,[cv2.IMWRITE_PNG_COMPRESSION, 0])
-                # cv2.imwrite("Test/labels_%03d.png" % i,torch.reshape(labels, (512, 512)).cpu().detach().numpy() * 255,[cv2.IMWRITE_PNG_COMPRESSION, 0])
                 optimizer.zero_grad()  # 梯度置零
-                predict = net(input_data)  # 数据输入网络输出预测值
+                predict = self.model(input_data)  # 数据输入网络输出预测值
                 loss = loss_function(predict, labels)  # 通过预测值与标签算出误差
                 loss.backward()  # 误差逆传播
                 optimizer.step()  # 通过梯度调整参数
                 Loss += loss
-            # os.system("nvidia-smi")
-            print(Loss.item())
-        return net
+            # print(Loss.item())
+        if model_name is None:
+            torch.save(self.model, self.model_path)
+        else:
+
+            torch.save(self.model, self.model_path)
 
 
 if __name__ == '__main__':
